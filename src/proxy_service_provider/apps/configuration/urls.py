c661b8a81ebbb00139bf1a4562adf82341fc5af2
musfiqurcse <md.musfiqurcse@outlook.com>
2020-06-18 12:33:44 +0200

    [Update] Model updated for databank project

--- src/proxy_service_provider/apps/configuration/urls.py
+++ src/proxy_service_provider/apps/configuration/urls.py
@@ -1,8 +1,15 @@
 from django.conf.urls import url
 from django.urls import include,path
 from rest_framework import routers
-
+from proxy_service_provider.apps.configuration.views.proxy_provider_views import ProxyProviderView
 
 
 
 router =  routers.SimpleRouter()
+router.register('proxy-service-provider', ProxyProviderView, basename='proxy-service-provider')
+
+
+
+urlpatterns = [
+    url(r'^', include(router.urls)),
+]
