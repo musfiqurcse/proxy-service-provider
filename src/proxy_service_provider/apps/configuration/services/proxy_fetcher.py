dffdb990b43661c26904f321ca0450e54134a9e7
musfiqurcse <md.musfiqurcse@outlook.com>
2020-06-20 16:58:44 +0200

    [Model] Design the model for testing the url:

--- src/proxy_service_provider/apps/configuration/services/proxy_fetcher.py
+++ src/proxy_service_provider/apps/configuration/services/proxy_fetcher.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from .selenium_driver import SeleniumDriver
+from fake_useragent import UserAgent
 import re
 
 
@@ -10,14 +11,45 @@ class ProxyFetching():
     def __init__(self):
         pass
 
+    def pass_proxy_test(self, proxy_address: str, proxy_port: str, ip_checker: str):
+        try:
+            # Initialize User Agent
+            ua = UserAgent()
+            concat_proxy_addres = proxy_address + ':'+ proxy_port
+            # Initialize Session
+            session_proxy = requests.Session()
+            # Configure UserAgent()
+            session_proxy.headers = {'User-Agent': ua.random}
+            # Configure Proxy
+            session_proxy.proxies = { 'https': 'https://'+concat_proxy_addres}
+            # Call IP Checker for Proxy Validity
+            res = session_proxy.get('https://httpbin.org/ip').json()
+            # Checking the Origin IP address from the request and Provider proxy is matched or not.
+            if res['origin'] == proxy_address:
+                return {'output': "Provided IP  and output from the check IP addess matched \n Input: {0}\nOutput:  {1} ".format(proxy_address,res['origin']),
+                        'result': True}
+            # As the output of the test IP address is not matched hence it is not passed the test cases.
+            return {
+                'output': "Provided IP and output from the check IP addess does not match\n Input: {0}\nOutput:  {1} ".format(proxy_address,res['origin']),
+                'result': False
+            }
+        except Exception as ex:
+            print('Error Occurred while testing proxy')
+            return {'output': ex, 'result': False}
+
     def fetch_data_from_proxy(self,url: str,multi_fetch=False,key_word='?page=',https_check=True):
+        # Configure the Selenium Web Driver
         driver = SeleniumDriver()
+        # Extrecting Data Using Selenium
         responses = driver.extract_url(url)
+        # Parsing The HTML as a Tag Basis using Beautiful Soup
         soup = BeautifulSoup(responses, 'lxml')
+        # Initialize the Proxy List
         proxies_list = []
         ip_address = ''
         port = ''
         indexer =  -1
+        cols = -1
         port_inclusive = False
         for item in soup.select("table tr"):
             # First td always hold the ip address, hence, we are extracting the first td and removing the unwanted space
@@ -25,10 +57,10 @@ class ProxyFetching():
             cond1 = 'yes' in item.text or 'HTTPS' in item.text or 'https' in item.text if not https_check else https_check
             cond2 = re.search("(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(?=[^\d])\s*", str(item))
             # cond2 =
-            cols = -1
             try:
                 if cond1 and cond2:
                     if cols > len(item.findAll('td')):
+                        continue
                     select_first_td = str(item.select_one('td').text).strip()
                     cols = len(item.findAll('td'))
                     if indexer == -1:
@@ -59,5 +91,8 @@ Debugging Shell Command
 from proxy_service_provider.apps.configuration.services.proxy_fetcher import ProxyFetching
 chk = ProxyFetching()
 chk.fetch_data_from_proxy('https://www.sslproxies.org/')
-https://codereview.stackexchange.com/questions/195842/web-scraping-through-a-rotating-proxy-script
+https://api.ipify.org?format=json
+https://httpbin.org/ip
+https://ipinfo.io/json
+
 """
